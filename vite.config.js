6eb4b4cc42c3e920d5a5eda96c9972deaeeff7a5
Juan anziano <juanianziano22@gmail.com>
2026-06-20 15:17:23 -0300

    listo el problema

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         chart: resolve(__dirname, "chart.html"),
-        exchange: resolve(__dirname, "exchange.html"),
+        exchange: resolve(__dirname, "exchange.html"), // ✅ singular, como tu archivo real
       },
     },
   },
